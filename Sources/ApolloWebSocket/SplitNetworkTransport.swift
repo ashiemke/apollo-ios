313923285b780bb3e236e6c151744a35141044b0
Adam Shiemke <ashiemke@tangogroup.com>
2018-11-14 23:32:45 -0700

    add web socket network conformance to APQ stuff

--- Sources/ApolloWebSocket/SplitNetworkTransport.swift
+++ Sources/ApolloWebSocket/SplitNetworkTransport.swift
@@ -9,11 +9,11 @@ public class SplitNetworkTransport: NetworkTransport {
     self.webSocketNetworkTransport = webSocketNetworkTransport
   }
   
-  public func send<Operation>(operation: Operation, completionHandler: @escaping (GraphQLResponse<Operation>?, Error?) -> Void) -> Cancellable {
+    public func send<Operation>(operation: Operation, optimistic: Bool, completionHandler: @escaping (GraphQLResponse<Operation>?, Error?) -> Void) -> Cancellable where Operation : GraphQLOperation {
     if operation.operationType == .subscription {
-        return webSocketNetworkTransport.send(operation: operation, completionHandler: completionHandler)
+        return webSocketNetworkTransport.send(operation: operation, optimistic: false, completionHandler: completionHandler)
     } else {
-        return httpNetworkTransport.send(operation: operation, completionHandler: completionHandler)
+        return httpNetworkTransport.send(operation: operation, optimistic: true, completionHandler: completionHandler)
     }
   }
 }
--- Sources/ApolloWebSocket/WebSocketTransport.swift
+++ Sources/ApolloWebSocket/WebSocketTransport.swift
@@ -59,7 +59,7 @@ public class WebSocketTransport: NetworkTransport, WebSocketDelegate {
     self.websocket.connect()
   }
   
-  public func send<Operation>(operation: Operation, completionHandler: @escaping (_ response: GraphQLResponse<Operation>?, _ error: Error?) -> Void) -> Cancellable {
+public func send<Operation>(operation: Operation, optimistic: Bool, completionHandler: @escaping (GraphQLResponse<Operation>?, Error?) -> Void) -> Cancellable where Operation : GraphQLOperation {
     if let error = self.error {
       completionHandler(nil,error)
     }
